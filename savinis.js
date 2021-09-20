741dc2f42ce2bc9fa2b7c187fc8828a82755807d
luizbarueri <luizbarueri@gmail.com>
2021-09-20 15:36:06 -0300

    iniciando sistema de controle

---
+++ savinis.js
@@ -0,0 +1,28 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const qtdAluno = 0;
+
+rl.question('Quantidade de alunos? ', (qtdAluno) => {
+  // TODO: Log the answer in a database
+  console.log(`Quantidade de aluno informada: ${qtdAluno}`);
+  rl.close();
+  
+  for (let x = 0; x <= qtdAluno; x++) {
+    if(x == 0){
+        console.log("Numero: " + x + " é zero")
+    } else {
+        if (x % 2 == 0) {
+            console.log("Numero: " + x + " é par")
+        } else {
+            console.log("Numero: " + x + " é impar")
+        }
+    }
+}
+});
+
+
